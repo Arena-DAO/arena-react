1267bfa8f19f47b3c2472d812943f8f410aa5339
Gabriel Lopez <glopez11193@gmail.com>
2024-03-07 16:41:41 -0600

    format

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,22 @@ import million from "million/compiler";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-  output: "export",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
+	images: {
+		unoptimized: true,
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+	output: "export",
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	experimental: {
+		missingSuspenseWithCSRBailout: false,
+	},
 };
 
 const millionConfig = {
-  auto: true,
+	auto: true,
 };
 
 export default million.next(nextConfig, millionConfig);
