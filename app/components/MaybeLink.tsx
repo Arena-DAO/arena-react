becaf64b8a5fe21c817286b1ad9cecabf562feab
Gabriel Lopez <glopez11193@gmail.com>
2024-04-03 09:40:29 -0500

    Fix MaybeLink
    was missing ipfs gateway

--- app/components/MaybeLink.tsx
+++ app/components/MaybeLink.tsx
@@ -1,12 +1,15 @@
 import { Link } from "@nextui-org/react";
 import { withIpfsSupport } from "~/helpers/IPFSHelpers";
+import { useEnv } from "~/hooks/useEnv";
 
 interface MaybeLinkProps {
 	content: string;
 }
 
 const MaybeLink = ({ content }: MaybeLinkProps) => {
-	const maybeIpfsUrl = withIpfsSupport(content);
+	const { data: env } = useEnv();
+
+	const maybeIpfsUrl = withIpfsSupport(env.IPFS_GATEWAY, content);
 	const url =
 		maybeIpfsUrl && URL.canParse(maybeIpfsUrl)
 			? new URL(maybeIpfsUrl)
