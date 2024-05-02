53155d7e4ce9fa3bb366a4d0a1bd76f742490264
Gabriel Lopez <glopez11193@gmail.com>
2024-05-02 16:19:57 -0500

    View jailed leagues through jailhouse

--- app/dao/jailhouse/page.tsx
+++ app/dao/jailhouse/page.tsx
@@ -10,17 +10,19 @@ const Jailhouse = () => {
 	return (
 		<div className="space-y-4">
 			<h1 className="text-center text-5xl">Jailhouse</h1>
-			<Tabs
-				aria-label="Competition Modules"
-				disabledKeys={["leagues", "tournaments"]}
-			>
+			<Tabs aria-label="Competition Modules" disabledKeys={["tournaments"]}>
 				<Tab key="wagers" title="Wagers">
 					<CompetitionModuleSection
 						path="wager"
 						module_addr={env.ARENA_WAGER_MODULE_ADDRESS}
 					/>
 				</Tab>
-				<Tab key="leagues" title="Leagues" />
+				<Tab key="leagues" title="Leagues">
+					<CompetitionModuleSection
+						path="league"
+						module_addr={env.ARENA_LEAGUE_MODULE_ADDRESS}
+					/>
+				</Tab>
 				<Tab key="tournaments" title="Tournaments" />
 			</Tabs>
 		</div>
