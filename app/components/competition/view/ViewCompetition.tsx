5907a4c640e659cd43bef0cec83c33fcf50faf71
Lighteros <lightningdev722@gmail.com>
2024-05-17 10:34:05 -0400

    Fix GH comments

--- app/components/competition/view/ViewCompetition.tsx
+++ app/components/competition/view/ViewCompetition.tsx
@@ -187,6 +187,16 @@ const ViewCompetition = ({
 						host={competition.host}
 					/>
 				)}
+				{competition.is_expired &&
+					status !== "inactive" &&
+					status !== "pending" && (
+					<ProcessForm
+						moduleAddr={moduleAddr}
+						competitionId={competition.id}
+						setCompetitionStatus={setStatus}
+						is_expired
+					/>
+				)}
 				{competition.is_expired && status !== "inactive" && (
 					<ProcessForm
 						moduleAddr={moduleAddr}
