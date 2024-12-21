a6b7d2027e59dde4c5d87ace39192f6ce003b4e5
ismellike <glopez11193@gmail.com>
2024-12-20 18:26:55 -0600

    Use zod for env validation

--- src/hooks/useEnv.ts
+++ src/hooks/useEnv.ts
@@ -1,118 +1,91 @@
 import { useQuery } from "@tanstack/react-query";
+import { z } from "zod";
 
-export interface Env {
-	BECH32_PREFIX: string;
-	PFPK_URL: string;
-	API_URL: string;
-	DISCORD_API_URL: string;
-	WALLETCONNECT_PROJECT_ID: string;
-	BECH32_WALLET_LENGTH: number;
-	BECH32_CONTRACT_LENGTH: number;
-	DEFAULT_TEAM_VOTING_DURATION_TIME: number;
-	PAGINATION_LIMIT: number;
-	IBC_FUN: string;
-	JACKAL_PATH: string;
+// Schema without NEXT_PUBLIC_ prefix
+const envSchema = z.object({
+	CHAIN: z.string(),
+	DEFAULT_NATIVE: z.string(),
+	ENV: z.string(),
+	CODE_ID_ESCROW: z.coerce.number(),
+	CODE_ID_GROUP: z.coerce.number(),
+	ARENA_DAO_ADDRESS: z.string(),
+	ARENA_CORE_ADDRESS: z.string(),
+	ARENA_WAGER_MODULE_ADDRESS: z.string(),
+	ARENA_LEAGUE_MODULE_ADDRESS: z.string(),
+	ARENA_TOURNAMENT_MODULE_ADDRESS: z.string(),
+	ARENA_COMPETITION_ENROLLMENT_ADDRESS: z.string(),
+	ARENA_ABC_ADDRESS: z.string(),
+	ARENA_ABC_SUPPLY_DENOM: z.string(),
+	ARENA_ABC_RESERVE_DENOM: z.string(),
+	ARENA_PAYMENT_REGISTRY_ADDRESS: z.string(),
+	ARENA_TOKEN_GATEWAY_ADDRESS: z.string(),
+	ARENA_DISCORD_IDENTITY_ADDRESS: z.string().optional(),
+	DAO_DAO_URL: z.string(),
+	DOCS_URL: z.string(),
+	RPC_URL: z.string(),
+	FAUCET_URL: z.string().optional(),
+	BECH32_PREFIX: z.string(),
+	PFPK_URL: z.string(),
+	API_URL: z.string(),
+	DISCORD_API_URL: z.string(),
+	WALLETCONNECT_PROJECT_ID: z.string(),
+	BECH32_WALLET_LENGTH: z.coerce.number(),
+	BECH32_CONTRACT_LENGTH: z.coerce.number(),
+	DEFAULT_TEAM_VOTING_DURATION_TIME: z.coerce.number(),
+	PAGINATION_LIMIT: z.coerce.number(),
+	IBC_FUN: z.string(),
+	JACKAL_PATH: z.string(),
+});
 
-	CHAIN: string;
-	DEFAULT_NATIVE: string;
-	ENV: string;
-	CODE_ID_ESCROW: number;
-	CODE_ID_GROUP: number;
-	ARENA_DAO_ADDRESS: string;
-	ARENA_CORE_ADDRESS: string;
-	ARENA_WAGER_MODULE_ADDRESS: string;
-	ARENA_LEAGUE_MODULE_ADDRESS: string;
-	ARENA_TOURNAMENT_MODULE_ADDRESS: string;
-	ARENA_COMPETITION_ENROLLMENT_ADDRESS: string;
-	ARENA_ABC_ADDRESS: string;
-	ARENA_ABC_SUPPLY_DENOM: string;
-	ARENA_ABC_RESERVE_DENOM: string;
-	ARENA_PAYMENT_REGISTRY_ADDRESS: string;
-	ARENA_TOKEN_GATEWAY_ADDRESS: string;
-	ARENA_DISCORD_IDENTITY_ADDRESS?: string;
-	DAO_DAO_URL: string;
-	DOCS_URL: string;
-	RPC_URL: string;
-	FAUCET_URL?: string;
-}
-
-const checkEnv = (key: string | undefined): string => {
-	if (key === undefined || key === null) {
-		throw new Error(`Environment variable ${key} is missing`);
-	}
-	return key;
-};
-
-const checkEnvNumber = (key: string | undefined): number => {
-	const string = checkEnv(key);
-	const value = Number.parseInt(string);
-	if (Number.isNaN(value)) {
-		throw new Error(`Environment variable ${key} is not a number`);
-	}
-	return value;
-};
-
-function getEnv(): Env {
-	return {
-		DISCORD_API_URL: checkEnv(process.env.NEXT_PUBLIC_DISCORD_API_URL),
+// Function to build the env object and validate it
+const getEnv = () => {
+	const env = {
+		CHAIN: process.env.NEXT_PUBLIC_CHAIN,
+		DEFAULT_NATIVE: process.env.NEXT_PUBLIC_DEFAULT_NATIVE,
+		ENV: process.env.NEXT_PUBLIC_ENV,
+		CODE_ID_ESCROW: process.env.NEXT_PUBLIC_CODE_ID_ESCROW,
+		CODE_ID_GROUP: process.env.NEXT_PUBLIC_CODE_ID_GROUP,
+		ARENA_DAO_ADDRESS: process.env.NEXT_PUBLIC_ARENA_DAO_ADDRESS,
+		ARENA_CORE_ADDRESS: process.env.NEXT_PUBLIC_ARENA_CORE_ADDRESS,
+		ARENA_WAGER_MODULE_ADDRESS:
+			process.env.NEXT_PUBLIC_ARENA_WAGER_MODULE_ADDRESS,
+		ARENA_LEAGUE_MODULE_ADDRESS:
+			process.env.NEXT_PUBLIC_ARENA_LEAGUE_MODULE_ADDRESS,
+		ARENA_TOURNAMENT_MODULE_ADDRESS:
+			process.env.NEXT_PUBLIC_ARENA_TOURNAMENT_MODULE_ADDRESS,
+		ARENA_COMPETITION_ENROLLMENT_ADDRESS:
+			process.env.NEXT_PUBLIC_ARENA_COMPETITION_ENROLLMENT_ADDRESS,
+		ARENA_ABC_ADDRESS: process.env.NEXT_PUBLIC_ARENA_ABC_ADDRESS,
+		ARENA_ABC_SUPPLY_DENOM: process.env.NEXT_PUBLIC_ARENA_ABC_SUPPLY_DENOM,
+		ARENA_ABC_RESERVE_DENOM: process.env.NEXT_PUBLIC_ARENA_ABC_RESERVE_DENOM,
+		ARENA_PAYMENT_REGISTRY_ADDRESS:
+			process.env.NEXT_PUBLIC_ARENA_PAYMENT_REGISTRY_ADDRESS,
+		ARENA_TOKEN_GATEWAY_ADDRESS:
+			process.env.NEXT_PUBLIC_ARENA_TOKEN_GATEWAY_ADDRESS,
 		ARENA_DISCORD_IDENTITY_ADDRESS:
 			process.env.NEXT_PUBLIC_ARENA_DISCORD_IDENTITY_ADDRESS,
-		IBC_FUN: checkEnv(process.env.NEXT_PUBLIC_IBC_FUN),
-		API_URL: checkEnv(process.env.NEXT_PUBLIC_API_URL),
-		BECH32_PREFIX: checkEnv(process.env.NEXT_PUBLIC_BECH32_PREFIX),
-		PFPK_URL: checkEnv(process.env.NEXT_PUBLIC_PFPK_URL),
-		WALLETCONNECT_PROJECT_ID: checkEnv(
-			process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-		),
-		BECH32_WALLET_LENGTH: checkEnvNumber(
-			process.env.NEXT_PUBLIC_BECH32_WALLET_LENGTH,
-		),
-		BECH32_CONTRACT_LENGTH: checkEnvNumber(
-			process.env.NEXT_PUBLIC_BECH32_CONTRACT_LENGTH,
-		),
-		DEFAULT_TEAM_VOTING_DURATION_TIME: checkEnvNumber(
+		DAO_DAO_URL: process.env.NEXT_PUBLIC_DAO_DAO_URL,
+		DOCS_URL: process.env.NEXT_PUBLIC_DOCS_URL,
+		RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
+		FAUCET_URL: process.env.NEXT_PUBLIC_FAUCET_URL, // Optional
+		BECH32_PREFIX: process.env.NEXT_PUBLIC_BECH32_PREFIX,
+		PFPK_URL: process.env.NEXT_PUBLIC_PFPK_URL,
+		API_URL: process.env.NEXT_PUBLIC_API_URL,
+		DISCORD_API_URL: process.env.NEXT_PUBLIC_DISCORD_API_URL,
+		WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+		BECH32_WALLET_LENGTH: process.env.NEXT_PUBLIC_BECH32_WALLET_LENGTH,
+		BECH32_CONTRACT_LENGTH: process.env.NEXT_PUBLIC_BECH32_CONTRACT_LENGTH,
+		DEFAULT_TEAM_VOTING_DURATION_TIME:
 			process.env.NEXT_PUBLIC_DEFAULT_TEAM_VOTING_DURATION_TIME,
-		),
-		PAGINATION_LIMIT: checkEnvNumber(process.env.NEXT_PUBLIC_PAGINATION_LIMIT),
-		CHAIN: checkEnv(process.env.NEXT_PUBLIC_CHAIN),
-		DEFAULT_NATIVE: checkEnv(process.env.NEXT_PUBLIC_DEFAULT_NATIVE),
-		ENV: checkEnv(process.env.NEXT_PUBLIC_ENV),
-		JACKAL_PATH: checkEnv(process.env.NEXT_PUBLIC_JACKAL_PATH),
-		CODE_ID_ESCROW: checkEnvNumber(process.env.NEXT_PUBLIC_CODE_ID_ESCROW),
-		CODE_ID_GROUP: checkEnvNumber(process.env.NEXT_PUBLIC_CODE_ID_GROUP),
-		ARENA_DAO_ADDRESS: checkEnv(process.env.NEXT_PUBLIC_ARENA_DAO_ADDRESS),
-		ARENA_CORE_ADDRESS: checkEnv(process.env.NEXT_PUBLIC_ARENA_CORE_ADDRESS),
-		ARENA_WAGER_MODULE_ADDRESS: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_WAGER_MODULE_ADDRESS,
-		),
-		ARENA_LEAGUE_MODULE_ADDRESS: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_LEAGUE_MODULE_ADDRESS,
-		),
-		ARENA_TOURNAMENT_MODULE_ADDRESS: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_TOURNAMENT_MODULE_ADDRESS,
-		),
-		ARENA_COMPETITION_ENROLLMENT_ADDRESS: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_COMPETITION_ENROLLMENT_ADDRESS,
-		),
-		ARENA_ABC_ADDRESS: checkEnv(process.env.NEXT_PUBLIC_ARENA_ABC_ADDRESS),
-		ARENA_ABC_SUPPLY_DENOM: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_ABC_SUPPLY_DENOM,
-		),
-		ARENA_ABC_RESERVE_DENOM: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_ABC_RESERVE_DENOM,
-		),
-		ARENA_PAYMENT_REGISTRY_ADDRESS: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_PAYMENT_REGISTRY_ADDRESS,
-		),
-		ARENA_TOKEN_GATEWAY_ADDRESS: checkEnv(
-			process.env.NEXT_PUBLIC_ARENA_TOKEN_GATEWAY_ADDRESS,
-		),
-		DAO_DAO_URL: checkEnv(process.env.NEXT_PUBLIC_DAO_DAO_URL),
-		DOCS_URL: checkEnv(process.env.NEXT_PUBLIC_DOCS_URL),
-		RPC_URL: checkEnv(process.env.NEXT_PUBLIC_RPC_URL),
-		FAUCET_URL: process.env.NEXT_PUBLIC_FAUCET_URL,
+		PAGINATION_LIMIT: process.env.NEXT_PUBLIC_PAGINATION_LIMIT,
+		IBC_FUN: process.env.NEXT_PUBLIC_IBC_FUN,
+		JACKAL_PATH: process.env.NEXT_PUBLIC_JACKAL_PATH,
 	};
-}
+
+	return envSchema.parse(env);
+};
+
+export type Env = z.infer<typeof envSchema>;
 
 export const useEnv = () => {
 	return useQuery(["env"], () => getEnv(), {
