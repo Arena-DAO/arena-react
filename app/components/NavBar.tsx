9f87144398fdd527f5d750ee652dce40879c9695
ismellike <glopez11193@gmail.com>
2025-01-28 00:56:39 -0600

    Improve navbar
    add icons and ramp link

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -20,6 +20,14 @@ import { Image } from "@heroui/react";
 import NextImage from "next/image";
 import NextLink from "next/link";
 import { useMemo, useState } from "react";
+import {
+	BsBookFill,
+	BsCoin,
+	BsDropletFill,
+	BsHouseFill,
+	BsPersonFillLock,
+	BsTrophyFill,
+} from "react-icons/bs";
 import { type Env, useEnv } from "~/hooks/useEnv";
 import WalletConnectToggle from "./WalletConnectToggle";
 
@@ -30,22 +38,33 @@ const menuConfig = (env: Env) => {
 			key: "home",
 			text: "Home",
 			href: "/",
+			icon: <BsHouseFill />,
 		},
 		{
 			key: "compete",
 			text: "Compete",
 			href: "/compete",
+			icon: <BsTrophyFill />,
 		},
 		{
 			key: "jailhouse",
 			text: "Jailhouse",
 			href: "/dao/jailhouse",
+			icon: <BsPersonFillLock />,
 		},
 		{
 			key: "docs",
 			text: "Docs",
 			href: env.DOCS_URL,
 			isExternal: true,
+			icon: <BsBookFill />,
+		},
+		{
+			key: "kado",
+			text: "Convert Funds",
+			href: "https://app.kado.money/?onPayCurrency=USD&onPayAmount=200&onRevCurrency=USDC&offPayCurrency=USDC&offRevCurrency=USD&network=NEUTRON&onToAddress=X&offFromAddress=X&cryptoList=USDC&networkList=NEUTRON&apiKey=API_KEY&product=BUY",
+			isExternal: true,
+			icon: <BsCoin />,
 		},
 	];
 
@@ -56,6 +75,7 @@ const menuConfig = (env: Env) => {
 			text: "Faucet",
 			href: env.FAUCET_URL,
 			isExternal: true,
+			icon: <BsDropletFill />,
 		});
 	}
 
@@ -76,11 +96,11 @@ export default function AppNavbar() {
 					"bg-default-200/50 dark:bg-default-100/50": isMenuOpen,
 				}),
 				wrapper: "w-full justify-center bg-transparent",
-				item: "hidden md:flex font-semibold",
+				item: "hidden lg:flex font-semibold",
 			}}
 			height="60px"
 		>
-			<NavbarMenuToggle className="text-default-400 md:hidden" />
+			<NavbarMenuToggle className="text-default-400 lg:hidden" />
 			<NavbarBrand>
 				<Image
 					as={NextImage}
@@ -93,24 +113,25 @@ export default function AppNavbar() {
 				/>
 				<h3 className="ml-2 font-extrabold text-inherit">Arena DAO</h3>
 				{env.ENV === "development" && (
-					<Chip className="ml-2 hidden md:flex" size="sm">
+					<Chip className="ml-2 hidden lg:flex" size="sm">
 						testnet
 					</Chip>
 				)}
 			</NavbarBrand>
 
 			<NavbarContent
-				className="hidden h-11 gap-6 rounded-full border-default-200/20 border-small bg-background/60 px-4 shadow-medium backdrop-blur-md backdrop-saturate-150 md:flex dark:bg-default-100/50"
+				className="hidden h-11 gap-6 rounded-full border-default-200/20 border-small bg-background/60 px-4 shadow-medium backdrop-blur-lg backdrop-saturate-150 lg:flex dark:bg-default-100/50"
 				justify="center"
 			>
 				{menuItems.map((item) => (
 					<NavbarItem key={item.key}>
 						<Link
 							as={NextLink}
-							className="text-default-500"
+							className="flex items-center gap-1 text-default-500"
 							href={item.href}
 							isExternal={item.isExternal}
 						>
+							{item.icon && item.icon}
 							{item.text}
 						</Link>
 					</NavbarItem>
@@ -123,7 +144,7 @@ export default function AppNavbar() {
 				</NavbarItem>
 			</NavbarContent>
 			<NavbarMenu
-				className="top-[calc(var(--navbar-height)_-_1px)] max-h-[70vh] bg-default-200/50 pt-6 shadow-medium backdrop-blur-md backdrop-saturate-150 dark:bg-default-100/50"
+				className="top-[calc(var(--navbar-height)_-_1px)] max-h-[70vh] bg-default-200/50 pt-6 shadow-medium backdrop-blur-lg backdrop-saturate-150 dark:bg-default-100/50"
 				motionProps={{
 					initial: { opacity: 0, y: -20 },
 					animate: { opacity: 1, y: 0 },
@@ -138,12 +159,13 @@ export default function AppNavbar() {
 					<NavbarMenuItem key={item.key}>
 						<Link
 							as={NextLink}
-							className="w-full text-default-500"
+							className="flex w-full items-center gap-1 text-default-500"
 							href={item.href}
 							size="md"
 							onPress={() => setIsMenuOpen(false)}
 							isExternal={item.isExternal}
 						>
+							{item.icon && item.icon}
 							{item.text}
 						</Link>
 					</NavbarMenuItem>
